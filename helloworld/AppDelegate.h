198ce4c94e7ee27f8e20e146991938bd19237403
scott <scott@scottdeMacBook-Pro.local>
2017-03-12 14:04:43 +0800

    Initial Commit

---
+++ helloworld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  helloworld
+//
+//  Created by scott on 2017/3/12.
+//  Copyright © 2017年 scott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
